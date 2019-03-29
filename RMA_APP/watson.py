ebce36992e724ccccc4209ec5bbd11501d938261
Cam K Weston <cameron.weston@uvm.edu>
2019-03-29 15:11:22 -0400

    Configured and implemented IBM Watson :)

--- RMA_APP/watson.py
+++ RMA_APP/watson.py
@@ -1,7 +1,7 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 
-API_KEY = "< uLc1SwHKUHMkNikCTgO1shd0I5KbbcoUzmfn_wmLjlFU >"
+API_KEY = "uLc1SwHKUHMkNikCTgO1shd0I5KbbcoUzmfn_wmLjlFU"
 URL = "https://gateway.watsonplatform.net/tone-analyzer/api"
 
 
@@ -17,6 +17,6 @@ def analyze_tone(text):
     )
 
     # TODO: This is where the error is
-    tone_analysis = tone_analyzer.tone({'text': text}, 'application/json').get_result()
+    tone_analysis = tone_analyzer.tone({'text': text}, 'application/json', False).get_result()
 
-    return json.dumps(tone_analysis, indent=2)
+    return json.dumps(tone_analysis, indent=4, sort_keys=True)
