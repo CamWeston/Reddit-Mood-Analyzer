fe90bb1e26dcfb82d20dee6d278328c416f89c43
NatYoungren <40671200+NatYoungren@users.noreply.github.com>
2019-04-29 21:33:01 -0400

    Removed old comments

--- RMA_APP/watson.py
+++ RMA_APP/watson.py
@@ -4,10 +4,6 @@ from watson_developer_cloud import ToneAnalyzerV3
 API_KEY = "uLc1SwHKUHMkNikCTgO1shd0I5KbbcoUzmfn_wmLjlFU"
 URL = "https://gateway.watsonplatform.net/tone-analyzer/api"
 
-
-# TODO: Fix the SSL error gonna take some googling -Cam
-
-
 # IBM Watson connection. This method is called from home.py
 def analyze_tone(text):
     tone_analyzer = ToneAnalyzerV3(
@@ -16,7 +12,6 @@ def analyze_tone(text):
         url=URL
     )
 
-    # TODO: This is where the error is
     tone_analysis = tone_analyzer.tone({'text': text}, 'application/json', False).get_result()
 
     return json.dumps(tone_analysis, indent=2, sort_keys=True)
