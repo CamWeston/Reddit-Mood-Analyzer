edda8fa590e732dfbdc7c2addf722b6f432286c6
CamWeston <ckweston@uvm.edu>
2019-05-30 06:47:50 -0700

    Update azure.py

--- RMA_APP/azure.py
+++ RMA_APP/azure.py
@@ -4,8 +4,8 @@ import re
 from IPython.display import HTML
 
 
-subscription_key = "71ce66c08c554bfda552d85b9eb168a8"
-sentiment_api_url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+subscription_key = "Insert your key here"
+sentiment_api_url = "Insert your key here"
 
 
 def analyze(text):
